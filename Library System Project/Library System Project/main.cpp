7d0263197a67dda502422c66f3d5b140bf21ad6f
yousef emad <yousefemadkhazbak@gmail.com>
2024-03-29 02:01:26 +0200

    project

---
+++ Library System Project/Library System Project/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Library System Project
+//
+//  Created by Yousef Emad on 29/03/2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
